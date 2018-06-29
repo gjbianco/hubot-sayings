99ec9860f9b903ccae06e9511ee3ad993f802bd5
Guy Bianco IV <gjbiancoiv@gmail.com>
2018-06-28 22:09:59 -0400

    initial version

---
+++ src/hubot-sayings.js
@@ -0,0 +1,23 @@
+const SAYINGS_KEY = process.env.HUBOT_SAYINGS_KEY || 'hubot-sayings'
+
+function getKey (key) {
+  return `${SAYINGS_KEY}-${key}`
+}
+
+module.exports = (robot) => {
+
+  robot.hear(/^!remember (\w+) (.+)$/i, (res) => {
+    robot.brain.set(getKey(res.match[1]), res.match[2])
+    res.send('okay, i\'ll remember that')
+  })
+
+  robot.hear(/^!recall (\w+)$/i, (res) => {
+    res.send(robot.brain.get(getKey(res.match[1])) || 'i don\'t know that')
+  })
+
+  robot.hear(/^!forget (\w+)$/i, (res) => {
+    robot.brain.set(getKey(res.match[1]), undefined)
+    res.send('forgotten')
+  })
+
+}
